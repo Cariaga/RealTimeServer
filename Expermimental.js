6e5fcb1f6a0db9fd21093431ce25d0e35ebd964c
Kaivaan <cariaga.info@gmail.com>
2019-10-18 09:26:50 +0800

    test

---
+++ Expermimental.js
@@ -0,0 +1,26 @@
+var data = {
+    file: '/home/johnlennon/walrus.png',
+    content_type: 'image/png'
+  };
+
+  var needle = require('needle');
+needle
+  .post('https://my.server.com/foo', data, { multipart: true })
+  .on('readable', function() { /* eat your chunks */ })
+  .on('done', function(err, resp) {
+    console.log('Ready-o!');
+  });
+
+  var redis = require('redis').createClient()
+var CronJob = require('cron-cluster')(redis).CronJob
+
+  function doCron () {
+    var job = new CronJob({
+      cronTime: '* * * * * *', 
+      onTick: function () {
+          // Do some stuff here
+      }
+    })
+    job.start()
+  }
+  
--- app.js
+++ app.js
@@ -1,41 +1,25 @@
 const app = require('express')();
 //exports.app = app;
-var needle = require('needle');
-
-
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const redisAdapter = require('socket.io-redis');
 io.adapter(redisAdapter({ host: 'localhost', port: 6379 }));
-
 const securitySetting=  require('./Shared/securitySetting');
-
-
-
 var redis = require('redis').createClient()
-var CronJob = require('cron-cluster')(redis).CronJob
  
-function doCron () {
-  var job = new CronJob({
-    cronTime: '* * * * * *', 
-    onTick: function () {
-        // Do some stuff here
-    }
-  })
-  job.start()
-}
-
-
 app.get('/', function (req, res) { res.status(200).send("test"); });
-
-
 
 io.on('connection', (socket) => { 
 console.log("connected");
-
-socket.on('message', function (data) {
-  console.log(data);
+socket.on('GPS', function (data) {
 });
+socket.on('Chip', function (data) {
+});
+socket.on('RacerToken', function (data) {
+  //get the connected PAD check if it is accociated with the RacerToken
+});
+
+
 
 
 socket.on('disconnect', function () { });
@@ -43,17 +27,7 @@ socket.on('disconnect', function () { });
 
 
 
-var data = {
-    file: '/home/johnlennon/walrus.png',
-    content_type: 'image/png'
-  };
 
-needle
-  .post('https://my.server.com/foo', data, { multipart: true })
-  .on('readable', function() { /* eat your chunks */ })
-  .on('done', function(err, resp) {
-    console.log('Ready-o!');
-  });
 
 app.use(function (err, req, res, next) {
     // handle error
