5ce117c3581e4c9b87c5409b6ece906da433f3d2
Kaivaan <cariaga.info@gmail.com>
2019-10-22 14:44:47 +0800

    test

--- Shared/game.js
+++ Shared/game.js
@@ -15,22 +15,4 @@ module.exports.JoinRace = function JoinRace(){//when joined the
 
 }
 
-////==========
-//this are private functions they are never public with export
-function FindAbstractKey(AbstractKey){//can be used to filter just the Pigeon and get all its properties
 
-}
-function GetAllPegionAssociationIDGameID(AssociationID,GameID){//Dual keys to make sure its for the association
-
-}
-function GetAllPegionDeviceID(DeviceID){//could be used to get all pegions pointing on one loft
-
-}
-function PigeonTimeStop(AbstractKey){//Finish and set time
-
-}
-function ClearGame(GameID){// Clean up memory this completely ends the game you can only access information via api now
-
-}
-
-///==========
---
+++ test/IntegrationTest.js
@@ -0,0 +1,32 @@
+var expect    = require("chai").expect;
+var should    = require("chai").should;
+describe("Real Time Server", function() {
+    describe("Expected Data", function() {
+    it("Try check expected data from Object to string",async function() {
+        const byteof=require('byteof');
+        const safeStringify = require('fast-safe-stringify')
+        var PigeonProperties ={//the key used in redis is the abstracted key that is 1 to one with the chip
+            P:"asdafasdfgasdfca",
+            G:"asdfasca",//GAME
+            P:"asdafasdfgasdfca",//Pigeon ChipID // the key represting the written key on chip
+            L:"asfasfvasv,asfasfasf",//Location
+            FT:"10/10/10 20:20PM",//FinishedTime
+            DID:"asdfvcsfwtflpofk",//DeviceID
+            As:"asdfocps",
+        };
+        let array =[];
+        let bytes=0;
+        for(var i =0;i<10000;i++){
+
+            bytes=bytes+byteof(safeStringify(PigeonProperties));
+
+        // array.push(PigeonProperties);
+        }
+        expect(bytes).equals(2620000);
+            });
+    });
+});
+
+
+
+
